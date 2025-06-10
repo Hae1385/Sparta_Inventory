8ef22a53ba1856279a2e760fb4f9a0f8ebdc1a4a
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-10 19:27:06 +0900

    Add : 아이템 스텟 정보창 추가

---
+++ Assets/Scripts/ItemStatValue.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ItemStatValue : MonoBehaviour
+{
+    public TextMeshProUGUI statNameText;
+    public TextMeshProUGUI statValueText;
+
+    public void SetStat(string name, float value)
+    {
+        statNameText.text = name;
+        statValueText.text = value.ToString();
+    }
+}
