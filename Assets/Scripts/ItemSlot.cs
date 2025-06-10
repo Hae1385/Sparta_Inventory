d90b779669fa49fbaf99c3b6a2d366ab00a6d311
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-10 19:26:33 +0900

    Add : 인벤토리 슬롯 추가

---
+++ Assets/Scripts/ItemSlot.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSlot : MonoBehaviour
+{
+    public ItemData ItemData;
+    public Image icon;
+    public TextMeshProUGUI quantityText;
+    private Outline outline;
+
+    public UIInventory inventory;
+
+    public int index;
+    public bool isEquipped;
+    public int quantity;
+
+    private void Awake()
+    {
+        outline = GetComponent<Outline>();
+    }
+
+    private void OnEnable()
+    {
+        outline.enabled = isEquipped;
+    }
+
+    public void InItem()
+    {
+        icon.gameObject.SetActive(true);
+        icon.sprite = ItemData.icon;
+        quantityText.text = quantity > 1 ? quantity.ToString() : string.Empty;
+
+        if (outline != null)
+        {
+            outline.enabled = isEquipped;
+            if (isEquipped)
+                quantityText.text = "E";
+        }
+    }
+
+    public void OutItme()
+    {
+        ItemData = null;
+        icon.gameObject.SetActive(false);
+        quantityText.text = string.Empty;
+    }
+
+    public void OnClickButton()
+    {
+        inventory.SelectItem(index);
+    }
+}
