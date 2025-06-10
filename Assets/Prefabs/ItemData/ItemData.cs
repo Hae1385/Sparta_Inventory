4962e2c867cf4a1e9f1a5e812593b0663b83b2bc
Hae1385 <206494390+Hae1385@users.noreply.github.com>
2025-06-10 19:27:41 +0900

    Add : 아이템 종류 추가

---
+++ Assets/Prefabs/ItemData/ItemData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public enum ItemType
+{
+    Equipable,
+    Resource
+}
+
+public enum EquimentType
+{
+    Weapon,
+    UpperAmor,
+    LowerAmor
+}
+
+[Serializable]
+public class ItemDataEquip
+{
+    public EquimentType Type;
+    public int atk;
+    public int def;
+    public float crit;
+    public int hp;
+}
+
+[CreateAssetMenu(fileName = "Item", menuName = "New Item")]
+public class ItemData : ScriptableObject
+{
+    [Header("Info")]
+    public string ItemName;
+    public string description;
+    public ItemType type;
+    public Sprite icon;
+
+    [Header("Stacking")]
+    public bool canStack;
+    public int maxStackAmount;
+
+    [Header("Equip")]
+    public ItemDataEquip EquipStat;
+}
